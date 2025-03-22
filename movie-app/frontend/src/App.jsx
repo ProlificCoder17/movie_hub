3058552610e6bcd7ee91b550f73a01561b79fc61
Tatenda <fitasco@gmail.com>
2025-03-22 02:45:18 +0000

    movie context update

--- movie-app/frontend/src/App.jsx
+++ movie-app/frontend/src/App.jsx
@@ -2,25 +2,24 @@ import "./css/App.css";
 import Moviecard from "./components/MovieCard";
 import Favorites from "./pages/Favorites";
 import Home from "./pages/Home";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div>
+    <MovieProvider>
       <NavBar />
       <main className="main-content">
-      <Routes>
-         {/* Define Home route explicitly */}
-         <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-        {/* Fallback route for undefined paths */}
-        <Route path="*" element={<div>Page Not Found</div>} />
-      </Routes>
-    </main>
-
-    </div>
-    
+        <Routes>
+          {/* Define Home route explicitly */}
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          {/* Fallback route for undefined paths */}
+          <Route path="*" element={<div>Page Not Found</div>} />
+        </Routes>
+      </main>
+    </MovieProvider>
   );
 }
 
